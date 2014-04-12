38a87694844e23a2dcff0c3be3a43c54e5cdc458
Natalie Low <gnatann@gmail.com>
2014-04-12 15:49:37 -0700

    Login/Logout plus styling new Message Board

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MessageBoard2::Application.config.secret_key_base = 'e1667f9e47f60c007853079752beb5935cf3cd6d900aa972cd324eabf6389c3581196e482da4ce65ea2076523b56b73470932d1e74836ab7623bcc5d7e1de9a5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MessageBoard2::Application.config.session_store :cookie_store, key: '_message_board2_session'
